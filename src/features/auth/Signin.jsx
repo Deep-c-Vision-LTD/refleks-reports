ac8e76bd382785bc373ffead616937c8994d48b6
jbarszczewski <jakub.barszczewski@outlook.com>
2025-03-11 22:19:57 +0100

    refactor: remove App component and enhance Signin and Dashboard components with improved UI and export functionality

--- src/features/auth/Signin.jsx
+++ src/features/auth/Signin.jsx
@@ -34,30 +34,74 @@ const Signin = () => {
 
   return (
     <div className="flex items-center justify-center min-h-screen bg-base-200">
-      <div className="w-full max-w-sm p-8 bg-white rounded-lg shadow-md">
+      <div className="w-full max-w-sm p-8 bg-base-300 rounded-lg shadow-md">
         <h1 className="mb-6 text-3xl font-bold text-center">Refleks Reports</h1>{" "}
         {/* App name header */}
         <form onSubmit={handleSignIn}>
           <h2 className="mb-6 text-2xl font-bold text-center">Sign in</h2>
           <div className="mb-4">
-            <input
-              onChange={(e) => setEmail(e.target.value)}
-              className="w-full p-3 border rounded-md"
-              type="email"
-              name="email"
-              id="email"
-              placeholder="Email"
-            />
+            <label className="input validator join-item">
+              <svg
+                className="h-[1em] opacity-50"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+              >
+                <g
+                  strokeLinejoin="round"
+                  strokeLinecap="round"
+                  strokeWidth="2.5"
+                  fill="none"
+                  stroke="currentColor"
+                >
+                  <rect width="20" height="16" x="2" y="4" rx="2"></rect>
+                  <path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7"></path>
+                </g>
+              </svg>
+              <input
+                onChange={(e) => setEmail(e.target.value)}
+                type="email"
+                name="email"
+                id="email"
+                placeholder="Email"
+                required
+              />
+            </label>
+            <div className="validator-hint hidden">
+              Enter valid email address
+            </div>
           </div>
           <div className="mb-4">
-            <input
-              onChange={(e) => setPassword(e.target.value)}
-              className="w-full p-3 border rounded-md"
-              type="password"
-              name="password"
-              id="password"
-              placeholder="Password"
-            />
+            <label class="input">
+              <svg
+                class="h-[1em] opacity-50"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+              >
+                <g
+                  stroke-linejoin="round"
+                  stroke-linecap="round"
+                  stroke-width="2.5"
+                  fill="none"
+                  stroke="currentColor"
+                >
+                  <path d="M2.586 17.414A2 2 0 0 0 2 18.828V21a1 1 0 0 0 1 1h3a1 1 0 0 0 1-1v-1a1 1 0 0 1 1-1h1a1 1 0 0 0 1-1v-1a1 1 0 0 1 1-1h.172a2 2 0 0 0 1.414-.586l.814-.814a6.5 6.5 0 1 0-4-4z"></path>
+                  <circle
+                    cx="16.5"
+                    cy="7.5"
+                    r=".5"
+                    fill="currentColor"
+                  ></circle>
+                </g>
+              </svg>
+
+              <input
+                onChange={(e) => setPassword(e.target.value)}
+                type="password"
+                name="password"
+                id="password"
+                placeholder="Password"
+              />
+            </label>
           </div>
           <button className="w-full p-3 mt-4 text-white bg-blue-500 rounded-md hover:bg-blue-600">
             Sign In
--- src/features/dashboard/Dashboard.jsx
+++ src/features/dashboard/Dashboard.jsx
@@ -1,26 +1,92 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-
 import { UserAuth } from "../auth/context/AuthContext";
 import Header from "./components/Header";
 
+import { supabase } from "../../supabaseClient";
 const Dashboard = () => {
+  const [resultsType, setResultsType] = useState("");
+  const [fromDate, setFromDate] = useState("");
+  const [toDate, setToDate] = useState("");
+  const [error, setError] = useState("");
+
   const { session } = UserAuth();
   const navigate = useNavigate();
-
-  //const [error, setError] = useState("");
 
   // Redirect to the sign-in page if there's no session
   if (!session) {
     navigate("/");
   }
 
+  const handleExport = async (e) => {
+    e.preventDefault();
+    console.log("Exporting data...");
+    // log form data
+    console.log(resultsType, fromDate, toDate);
+    const { data, err } = await supabase.from(resultsType).select().limit(10);
+    if (err) {
+      console.error("Error fetching results:", err);
+      setError(err);
+      return;
+    }
+    console.log(data);
+  };
+
   return (
     <div>
       <Header />
-      <h2 className="text-2xl text-gray-800 mt-5">
-        Welcome, {session?.user?.email}
-      </h2>
+      <div className="flex items-center justify-center min-h-screen bg-base-200">
+        <div className="w-full max-w-sm p-8 bg-base-300 rounded-lg shadow-md">
+          <form onSubmit={handleExport}>
+            <div className="form-control flex flex-row justify-center space-x-16 mb-4">
+              <label className="label cursor-pointer">
+                <span className="label-text">GPPAQ</span>
+                <input
+                  type="radio"
+                  name="resultsType"
+                  className="radio checked:bg-blue-500"
+                  value="gppaq_entries"
+                  onChange={(e) => setResultsType(e.target.value)}
+                />
+              </label>
+              <label className="label cursor-pointer">
+                <span className="label-text">WOMAC</span>
+                <input
+                  type="radio"
+                  name="resultsType"
+                  className="radio checked:bg-blue-500"
+                  value="womac_entries"
+                  onChange={(e) => setResultsType(e.target.value)}
+                />
+              </label>
+            </div>
+            <div className="form-control mb-4">
+              <label className="label">
+                <span className="label-text">From</span>
+              </label>
+              <input
+                type="date"
+                className="input input-bordered"
+                onChange={(e) => setFromDate(e.target.value)}
+              />
+            </div>
+            <div className="form-control mb-4">
+              <label className="label">
+                <span className="label-text">To</span>
+              </label>
+              <input
+                type="date"
+                className="input input-bordered"
+                onChange={(e) => setToDate(e.target.value)}
+              />
+            </div>
+            <button type="submit" className="btn btn-primary mt-5">
+              Submit
+            </button>
+          </form>
+          {error && <p className="text-red-600 text-center pt-4">{error}</p>}
+        </div>
+      </div>
     </div>
   );
 };
