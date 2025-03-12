62cb34a68f62830089143cc56bb55e2548cc2a27
jbarszczewski <jakub.barszczewski@outlook.com>
2025-03-12 12:11:12 +0100

    feat: enhance data export functionality in Dashboard component to include date filtering and CSV download

--- src/features/dashboard/Dashboard.jsx
+++ src/features/dashboard/Dashboard.jsx
@@ -23,13 +23,35 @@ const Dashboard = () => {
     console.log("Exporting data...");
     // log form data
     console.log(resultsType, fromDate, toDate);
-    const { data, err } = await supabase.from(resultsType).select().limit(10);
+    const { data, err } = await supabase
+      .from(resultsType)
+      .select()
+      .gte("updated_at", fromDate)
+      .lte("updated_at", toDate + "T23:59:59.999Z") // add time to toDate to include whole day
+      .limit(10)
+      .order("updated_at");
     if (err) {
       console.error("Error fetching results:", err);
       setError(err);
       return;
     }
     console.log(data);
+    // export as CSV file
+    const csv = data.map((entry) => {
+      return Object.values(entry).join(",");
+    });
+    const headers =
+      resultsType == "gppaq_entries"
+        ? "id,created_at,updated_at,submitted_at,user_id,is_device_data,q1,q2,q3,q4,q5,device_q1,device_q2,device_q3,device_q4_5,compound,naids,painkillers,walking_pace,physical_activity_at_work,device_stair_ascent_speed,device_stair_descent_speed,device_stand_time,device_walking_asymmetry_percentage,device_walking_double_support_percentage,device_walking_speed"
+        : "id,created_at,updated_at,submitted_at,user_id,pain_q1,pain_q2,pain_q3,pain_q4,pain_q5,stiffness_q1,stiffness_q2,pf_q1,pf_q2,pf_q3,pf_q4,pf_q5,pf_q6,pf_q7,pf_q8,pf_q9,pf_q10,pf_q11,pf_q12,pf_q13,pf_q14,pf_q15,pf_q16,pf_q17,qol_q1,qol_q2,qol_q3,qol_q4,qol_q5";
+    const csvString = headers + "\n" + csv.join("\n");
+    const blob = new Blob([csvString], { type: "text/csv" });
+    const url = window.URL.createObjectURL(blob);
+    const a = document.createElement("a");
+    a.href = url;
+    const currentDateTime = new Date().toISOString().replace(/[:.]/g, "-");
+    a.download = `${resultsType}-${currentDateTime}.csv`;
+    a.click();
   };
 
   return (
